9c32f11f9cb3d2230a258e9f9281cd634d57acd3
bntrtm <git.bntrtm@tjbainter.com>
2026-01-11 01:11:55 -0600

    feat(cfg): add ISO field, sm tags to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,9 +6,10 @@ import (
 )
 
 type ConfigSettings struct {
-	BaseURL        string `json:"db_url" smname:"Database URL"`
-	StayLoggedIn   bool   `json:"stay_logged_in" smname:"Stay Logged In" smdes:"Keep a login session alive on exit."`
-	VimKeysEnabled bool   `json:"vim_keys_enabled" smname:"Vim Keys Enabled" menudes:"Use vim keys to navigate CLI menus."`
+	BaseURL         string `json:"db_url" smname:"Database URL" smdes:"URL of the server to connect to"`
+	StayLoggedIn    bool   `json:"stay_logged_in" smname:"Stay Logged In" smdes:"Keep a login session alive on exit."`
+	CurrencyISOCode string `json:"currency_iso_code" smname:"Currency ISO" smdes:"The ISO Code of the currency desired for monetary visualization"`
+	VimKeysEnabled  bool   `json:"vim_keys_enabled" smname:"Vim Keys Enabled" smdes:"Use vim keys to navigate CLI menus."`
 }
 
 // Config represents a configuration specific to the local machine.
@@ -28,9 +29,10 @@ func (c *Config) NewConfigFile(dbURL string) error {
 
 func (c *Config) SetDefaults(dbURL string) {
 	c.ConfigSettings = ConfigSettings{
-		BaseURL:        dbURL,
-		StayLoggedIn:   true,
-		VimKeysEnabled: true,
+		BaseURL:         dbURL,
+		StayLoggedIn:    true,
+		CurrencyISOCode: "USD",
+		VimKeysEnabled:  true,
 	}
 }
 
