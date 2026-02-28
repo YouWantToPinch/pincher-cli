f35930532e4505434fcfff64a9c9b0cbf02d6c08
bntrtm <git.bntrtm@tjbainter.com>
2026-02-28 04:01:25 -0600

    refactor(client): build endpoints with hierarachical structure

--- internal/client/requests_budgets.go
+++ internal/client/requests_budgets.go
@@ -1,120 +1,40 @@
 package client
 
 import (
-	"fmt"
 	"net/http"
 )
 
-func (c *Client) CreateBudget(name, notes string) (success bool, error error) {
-	url := c.API() + "/budgets"
-
-	payload := Meta{
-		Name:  name,
-		Notes: notes,
-	}
-
-	resp, err := c.Post(url, c.token, payload, nil)
-	if err != nil {
-		return false, err
-	}
-
-	switch resp.StatusCode {
-	case http.StatusCreated:
-		return true, nil
-	default:
-		return false, fmt.Errorf("failed to create budget")
-	}
+func (c *Client) BudgetCreate(bID string, data BudgetCreateData) error {
+	endpoint := EndpointBudgets()
+	err := c.Request(http.MethodPost, endpoint, data, nil)
+	return err
 }
 
-func (c *Client) GetBudgets(urlQuery string) ([]Budget, error) {
-	url := c.API() + "/budgets" + urlQuery
-
-	type budgetContainer struct {
-		Budgets []Budget `json:"budgets"`
-	}
-
-	var budgets budgetContainer
-	resp, cached, err := c.Get(url, c.token, &budgets)
-	if err != nil {
-		return nil, err
-	} else if cached {
-		return budgets.Budgets, nil
-	}
-
-	switch resp.StatusCode {
-	case http.StatusOK:
-		return budgets.Budgets, nil
-	case http.StatusNotFound:
-		return nil, fmt.Errorf("resource not found")
-	default:
-		return nil, fmt.Errorf("failed to retrieve budgets")
-	}
+type budgetContainer struct {
+	Budgets []*Budget `json:"data"`
 }
 
-func (c *Client) GetBudgetReport(monthID string) (MonthReport, error) {
-	url := c.API() + "/budgets/" + c.ViewedBudget.ID.String() + "/months/" + monthID
-
-	var report MonthReport
-	resp, cached, err := c.Get(url, c.token, &report)
-	if err != nil {
-		return MonthReport{}, err
-	} else if cached {
-		return report, nil
-	}
-
-	switch resp.StatusCode {
-	case http.StatusOK:
-		return report, nil
-	case http.StatusBadRequest:
-		return MonthReport{}, fmt.Errorf("improper input")
-	default:
-		return MonthReport{}, fmt.Errorf("failed to get budget report")
-	}
+func (c *Client) Budgets(bID, urlQuery string) (budgets []*Budget, err error) {
+	endpoint := EndpointBudgets() + urlQuery
+	var container budgetContainer
+	err = c.Request(http.MethodGet, endpoint, nil, &container)
+	return container.Budgets, err
 }
 
-func (c *Client) UpdateBudget(budgetID, name, notes string) error {
-	url := c.API() + "/budgets/" + budgetID
-
-	type rqSchema struct {
-		Meta
-	}
-
-	payload := rqSchema{
-		Meta: Meta{
-			Name:  name,
-			Notes: notes,
-		},
-	}
-
-	resp, err := c.Put(url, c.token, payload)
-	if err != nil {
-		return err
-	}
-
-	switch resp.StatusCode {
-	case http.StatusNoContent:
-		return nil
-	case http.StatusNotFound:
-		return fmt.Errorf("resource not found")
-	default:
-		return fmt.Errorf("failed to update budget")
-	}
+func (c *Client) BudgetReport(bID, mID string) (report *MonthReport, err error) {
+	endpoint := EndpointBudgetMonth(bID, mID)
+	err = c.Request(http.MethodGet, endpoint, nil, &report)
+	return report, err
 }
 
-func (c *Client) DeleteBudget(budgetID string) error {
-	url := c.API() + "/budgets/" + budgetID
+func (c *Client) BudgetUpdate(bID string, data BudgetUpdateData) error {
+	endpoint := EndpointBudget(bID)
+	err := c.Request(http.MethodPut, endpoint, data, nil)
+	return err
+}
 
-	resp, err := c.Delete(url, c.token, nil)
-	if err != nil {
-		return err
-	}
-
-	switch resp.StatusCode {
-	case http.StatusNoContent:
-		return nil
-	case http.StatusNotFound:
-		return fmt.Errorf("resource not found")
-	default:
-		return fmt.Errorf("failed to delete budgets")
-	}
+func (c *Client) BudgetDelete(bID string) error {
+	endpoint := EndpointBudget(bID)
+	err := c.Request(http.MethodDelete, endpoint, nil, nil)
+	return err
 }
--- internal/client/requests_groups.go
+++ internal/client/requests_groups.go
@@ -1,104 +1,40 @@
 package client
 
 import (
-	"fmt"
 	"net/http"
 )
 
-func (c *Client) CreateGroup(name, notes string) (success bool, error error) {
-	type rqSchema struct {
-		Meta
-	}
-
-	url := c.API() + "/budgets/" + c.ViewedBudget.ID.String() + "/groups"
-	payload := rqSchema{
-		Meta: Meta{
-			Name:  name,
-			Notes: notes,
-		},
-	}
-
-	resp, err := c.Post(url, c.token, payload, nil)
-	if err != nil {
-		return false, err
-	}
-
-	switch resp.StatusCode {
-	case http.StatusCreated:
-		return true, nil
-	default:
-		return false, fmt.Errorf("failed to create group")
-	}
+func (c *Client) BudgetGroupCreate(bID string, data BudgetGroupCreateData) error {
+	endpoint := EndpointBudgetGroups(bID)
+	err := c.Request(http.MethodPost, endpoint, data, nil)
+	return err
 }
 
-func (c *Client) GetGroups(urlQuery string) ([]Group, error) {
-	url := c.API() + "/budgets/" + c.ViewedBudget.ID.String() + "/groups" + urlQuery
-
-	type groupContainer struct {
-		Groups []Group `json:"groups"`
-	}
-
-	var groups groupContainer
-	resp, cached, err := c.Get(url, c.token, &groups)
-	if err != nil {
-		return nil, err
-	} else if cached {
-		return groups.Groups, nil
-	}
-
-	switch resp.StatusCode {
-	case http.StatusOK:
-		return groups.Groups, nil
-	case http.StatusNotFound:
-		return nil, fmt.Errorf("resource not found")
-	default:
-		return nil, fmt.Errorf("failed to retrieve budget groups")
-	}
+type groupContainer struct {
+	Groups []*Group `json:"data"`
 }
 
-func (c *Client) UpdateGroup(groupID, name, notes string) error {
-	url := c.API() + "/budgets/" + c.ViewedBudget.ID.String() + "/groups/" + groupID
-
-	type rqSchema struct {
-		Meta
-	}
-
-	payload := rqSchema{
-		Meta: Meta{
-			Name:  name,
-			Notes: notes,
-		},
-	}
-
-	resp, err := c.Put(url, c.token, payload)
-	if err != nil {
-		return err
-	}
-
-	switch resp.StatusCode {
-	case http.StatusNoContent:
-		return nil
-	case http.StatusNotFound:
-		return fmt.Errorf("resource not found")
-	default:
-		return fmt.Errorf("failed to retrieve budget groups")
-	}
+func (c *Client) BudgetGroups(bID, urlQuery string) (groups []*Group, err error) {
+	endpoint := EndpointBudgetGroups(bID) + urlQuery
+	var container groupContainer
+	err = c.Request(http.MethodGet, endpoint, nil, &container)
+	return container.Groups, err
 }
 
-func (c *Client) DeleteGroup(groupID string) error {
-	url := c.API() + "/budgets/" + c.ViewedBudget.ID.String() + "/groups/" + groupID
+func (c *Client) BudgetGroupUpdate(bID, gID string, data BudgetGroupUpdateData) error {
+	endpoint := EndpointBudgetGroup(bID, gID)
+	err := c.Request(http.MethodPut, endpoint, data, nil)
+	return err
+}
 
-	resp, err := c.Delete(url, c.token, nil)
-	if err != nil {
-		return err
-	}
+func (c *Client) BudgetGroupRestore(bID, gID string) error {
+	endpoint := EndpointBudgetGroup(bID, gID)
+	err := c.Request(http.MethodPatch, endpoint, nil, nil)
+	return err
+}
 
-	switch resp.StatusCode {
-	case http.StatusNoContent:
-		return nil
-	case http.StatusNotFound:
-		return fmt.Errorf("resource not found")
-	default:
-		return fmt.Errorf("failed to retrieve budget groups")
-	}
+func (c *Client) BudgetGroupDelete(bID, gID string) error {
+	endpoint := EndpointBudgetGroup(bID, gID)
+	err := c.Request(http.MethodDelete, endpoint, nil, nil)
+	return err
 }
