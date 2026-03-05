9f89786e649fb54ba31884642240f1790bc119b3
bntrtm <git.bntrtm@tjbainter.com>
2026-03-04 23:02:35 -0600

    docs(client): add in-line docs for account requests
    The methodology for the other resources are the same; until docs are added for those, it is really all the same as this, so one could point to this documentation for a fairly good understanding of how all of these request functions would work for each resource.

--- internal/client/requests_accounts.go
+++ internal/client/requests_accounts.go
@@ -4,6 +4,11 @@ import (
 	"net/http"
 )
 
+// BudgetAccountCreate makes an API call to create an account with the given
+// data, belonging to the budget identified by the given budget ID.
+//
+// Any non-nil result is saved to the client's internal cache, assuming the
+// cache is enabled.
 func (c *Client) BudgetAccountCreate(bID string, data BudgetAccountCreateData) error {
 	endpoint := EndpointBudgetAccounts(bID)
 	var account *Account
@@ -16,6 +21,11 @@ type accountContainer struct {
 	Accounts []*Account `json:"data"`
 }
 
+// BudgetAccount makes an API call to get an account by ID
+// from the budget identified by the given budget ID.
+//
+// Any non-nil result is saved to the client's internal cache, assuming the
+// cache is enabled.
 func (c *Client) BudgetAccount(bID, aID string) (account *Account, err error) {
 	endpoint := EndpointBudgetAccount(bID, aID)
 	err = c.Request(http.MethodGet, endpoint, nil, &account)
@@ -23,6 +33,13 @@ func (c *Client) BudgetAccount(bID, aID string) (account *Account, err error) {
 	return account, err
 }
 
+// BudgetAccounts makes an API call to get accounts belonging to
+// the budget identified by the given budget ID.
+//
+// Any non-nil result is saved to the client's internal cache, assuming the
+// cache is enabled.
+// The urlQuery value is appended to the endpoint string to be stored within the
+// metadata of the cache entry as the Destination URL.
 func (c *Client) BudgetAccounts(bID, urlQuery string) (accounts []*Account, err error) {
 	endpoint := EndpointBudgetAccounts(bID) + urlQuery
 	var container accountContainer
@@ -31,6 +48,12 @@ func (c *Client) BudgetAccounts(bID, urlQuery string) (accounts []*Account, err 
 	return container.Accounts, err
 }
 
+// BudgetAccountUpdate makes an API call to update an account belonging to
+// the budget identified by the given budget ID with the given data.
+//
+// Any non-nil result is saved to the client's internal cache, assuming the
+// cache is enabled. With no error returned, a separate request is made to
+// fetch the account, so as to keep the cache in sync.
 func (c *Client) BudgetAccountUpdate(bID, aID string, data BudgetAccountUpdateData) error {
 	endpoint := EndpointBudgetAccount(bID, aID)
 	err := c.Request(http.MethodPut, endpoint, data, nil)
@@ -41,12 +64,20 @@ func (c *Client) BudgetAccountUpdate(bID, aID string, data BudgetAccountUpdateDa
 	return err
 }
 
+// BudgetAccountRestore makes an API call to update an account by ID belonging to
+// the budget identified by the given budget ID. The update, made via a
+// PATCH request, merely alters the account's deletion status from soft-deleted
+// to non-deleted.
 func (c *Client) BudgetAccountRestore(bID, aID string) error {
 	endpoint := EndpointBudgetAccount(bID, aID)
 	err := c.Request(http.MethodPatch, endpoint, nil, nil)
 	return err
 }
 
+// BudgetAccountDelete makes an API call to delete an account by ID belonging to
+// the budget identified by the given budget ID.
+//
+// Any existing copy of the item in the client's internal cache is deleted.
 func (c *Client) BudgetAccountDelete(bID, aID string, data BudgetAccountDeleteData) error {
 	endpoint := EndpointBudgetAccount(bID, aID)
 	err := c.Request(http.MethodDelete, endpoint, data, nil)
